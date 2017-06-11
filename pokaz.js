cc9afc90616d04672d2b337d9a3b7ca62e65721c
Aleksandr-Sklyarov <shura.sklyarov@yandex.ru>
2017-06-12 02:43:08 +0300

    Add files via upload

---
+++ pokaz.js
@@ -0,0 +1,72 @@
+
+  $(document).ready(function() { 
+      $("A#Opisanie").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      }); 
+
+
+           $("A#Opisanie2").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id2").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id2").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      });
+
+
+   $("A#Opisanie3").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id3").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id3").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      });
+
+  $("A#Opisanie4").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id4").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id4").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      });
+
+    $("A#Opisanie5").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id5").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id5").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      });
+
+  $("A#Opisanie6").toggle(function() { 
+        // Отображаем скрытый блок 
+        $("DIV#block_id6").fadeIn(1000); // fadeIn - плавное появление
+        return false; // не производить переход по ссылке
+      },  
+      function() { 
+        // Скрываем блок 
+        $("DIV#block_id6").fadeOut(); // fadeOut - плавное исчезновение 
+        return false; // не производить переход по ссылке
+      });
+
+    });  
+
